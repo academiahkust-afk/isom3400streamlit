9e48ce1be2ba4c32af58a9d239a83d039239e1c0
academiahkust-afk <academiahkust@gmail.com>
2025-11-24 14:23:16 +0800

    Enhance data handling with new DataFrame creation
    Added functionality to create and display a new DataFrame with product sales and customer data.

--- datahandling.py
+++ datahandling.py
@@ -3,13 +3,23 @@ import pandas as pd
 import os
 
 # Get the current working directory
-current_directory = os.getcwd()
+current_directory = os.getcwd()      #good practice to ensure you get the current directory 
 # Define the file path
 file_path = os.path.join(current_directory, 'winequality-red.csv')
 
 # Read the CSV file into a DataFrame
-df = pd.read_csv(file_path, delimiter=';')
+df = pd.read_csv(file_path, delimiter=';') #delimiter is the separator symbol that your csv file uses 
+#df is not just for data (rows and columns) but has a host of other functionality associated with dataframe objects 
 
 # Display the DataFrame in an interactive table
 st.write("Wine Quality Data")
 st.dataframe(df)
+
+data = {'Product':['A','B','C'],
+        'Sales':[1200,850,950],
+        'Customers':[300,400,350]}
+
+df2=pd.DataFrame(data)
+st.dataframe(df2)
+st.dataeditor(df2)
+st.table(df2)
