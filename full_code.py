a965635268f636054d2dacea5abad74b6f7e57bc
academiahkust-afk <academiahkust@gmail.com>
2025-12-17 16:58:32 +0800

    Create full_code.py

---
+++ full_code.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+st.header("ISOM3400: Business Dashboard")
+st.write("Practicing using Streamlit Functions to create an interactive Web App")
+
+with st.sidebar:
+  selected=option_menu(
+    menu_title="title",
+    options=["Home","Customer Feedback","Market Insight"],
+    icons=["1-circle-fill","2-circle-fill","3-circle-fill"],
+    default_index=0)
+  
